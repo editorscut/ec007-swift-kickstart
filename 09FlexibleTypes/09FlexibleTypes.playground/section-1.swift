28fac3d09f04195ba880a06db0c28660a14d6ea0
Daniel Steinberg <daniel@dimsumthinking.com>
2014-08-26 10:27:13 -0400

    Code is ready for book version 1.4

--- 09FlexibleTypes/09FlexibleTypes.playground/section-1.swift
+++ 09FlexibleTypes/09FlexibleTypes.playground/section-1.swift
@@ -1,27 +1,46 @@
-let numberOfHoursInStandardWeek = 40
-
-
-class Employee  {
-    var hourlyWage: Int
-    init(hourlyWage: Int) {
-        self.hourlyWage = hourlyWage
+struct Publisher {
+    var authors = [ String : Author ]()
+    func royaltiesOnFirstBookForAuthorWithName(authorName: String) -> Int {
+        return (authors[authorName]?.titles.first?.royalties() ?? 0)
     }
-    func weeklyPay() -> Int {
-        return 0
+    /* nice version that returns an optional
+    func royaltiesOnFirstBookForAuthorWithName(authorName: String) -> Int? {
+        return authors[authorName]?.titles.first?.royalties()
+    }
+*/
+    /* ugly version
+    func royaltiesOnFirstBookForAuthorWithName(authorName: String) -> Int? {
+        if let validAuthor = authors[authorName] {
+            if let validFirst = validAuthor.titles.first {
+                return validFirst.royalties()
+            } else {
+                return nil
+            }
+        } else {
+            return nil
+        }
+    }
+    */
+}
+
+struct Author {
+    var titles = [Title]()
+}
+
+struct Title {
+    let numberSold: Int
+    func royalties() -> Int {
+        return Int( 0.15 * Double(numberSold))
     }
 }
 
-class SalariedEmployee : Employee {
-    override func weeklyPay() -> Int {
-        return numberOfHoursInStandardWeek * hourlyWage
-    }
-}
+let murderByOptionals = Title(numberSold: 400)
+let oneHitWonder = Author(titles: [murderByOptionals])
 
-class HourlyEmployee : Employee {
-    var numberOfHours = 0
-    override func weeklyPay() -> Int {
-        return numberOfHours * hourlyWage
-    }
-}
+let aspiringWriter = Author()
 
+let publisher = Publisher(authors: ["Determined": aspiringWriter, "Fluff": oneHitWonder])
 
+publisher.royaltiesOnFirstBookForAuthorWithName("Determined")
+publisher.royaltiesOnFirstBookForAuthorWithName("Fluff")
+publisher.royaltiesOnFirstBookForAuthorWithName("Not here")
