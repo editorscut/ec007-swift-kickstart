2437b4442e4488a288b65b63f9faa577254dff9b
Daniel Steinberg <daniel@dimsumthinking.com>
2014-09-12 07:46:47 +0200

    Adding files for chapter 2 rewrite

---
+++ 02Functions/02Functions.playground/section-1.swift
@@ -0,0 +1,8 @@
+func helloWorld() {
+    println("Hello, World!")
+}
+func helloWorld(name: String) {
+    println("Hello, \(name)!")
+}
+helloWorld()
+helloWorld("Swift Programmer")
---
+++ 02Functions/03Functions.playground/section-1.swift
@@ -0,0 +1,8 @@
+func helloWorld(name: String) {
+    println("Hello, \(name)!")
+}
+func helloWorld() {
+    helloWorld("World")
+}
+helloWorld()
+helloWorld("Swift Programmer")
