afac07d6d0f567ca7073e6b44694987f8a69c0e8
Daniel Steinberg <daniel@dimsumthinking.com>
2014-09-15 17:49:49 -0400

    Had a file repeated - edited it to be correct.

--- 09FlexibleTypes/05FlexibleTypes.playground/section-1.swift
+++ 09FlexibleTypes/05FlexibleTypes.playground/section-1.swift
@@ -19,15 +19,7 @@ class SalariedEmployee : Employee {
 class HourlyEmployee : Employee {
     var numberOfHours = 0
     override func weeklyPay() -> Int {
-        switch numberOfHours {
-        case 0...numberOfHoursInStandardWeek where hourlyWage < 50:
-            return numberOfHours * hourlyWage + 30
-        case numberOfHoursInStandardWeek...(24 * 7):
-            let overtimeHours = numberOfHours - numberOfHoursInStandardWeek
-            return (numberOfHours + overtimeHours) * hourlyWage
-        default:
-            return numberOfHours * hourlyWage
-        }
+        return numberOfHours * hourlyWage
     }
 }
 
@@ -37,14 +29,6 @@ func payEmployee(employee : Employee) -> Int {
 
 let manager = SalariedEmployee(hourlyWage: 100)
 payEmployee(manager)
-
-let partTimeEngineer = HourlyEmployee(hourlyWage: 60)
-partTimeEngineer.numberOfHours = 30
-payEmployee(partTimeEngineer)
-
-let underpaidPartTimeEngineer = HourlyEmployee(hourlyWage: 40)
-underpaidPartTimeEngineer.numberOfHours = 30
-payEmployee(underpaidPartTimeEngineer)
 
 let engineer = HourlyEmployee(hourlyWage: 60)
 engineer.numberOfHours = 50
