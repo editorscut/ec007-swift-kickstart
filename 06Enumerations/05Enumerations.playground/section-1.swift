1bdb12e72893efd350d6517dae6bf6280109a11a
Daniel Steinberg <daniel@dimsumthinking.com>
2015-07-30 20:40:23 -0400

    Removed old chapter 6 material

--- 06Enumerations/05Enumerations.playground/section-1.swift
+++
@@ -1,7 +0,0 @@
-enum BasketballPosition: String {
-    case PointGuard = "point guard", ShootingGuard = "shooting guard"
-    case SmallForward = "small forward", PowerForward = "power forward"
-    case Center = "center"
-}
-let ballHandler = BasketballPosition.ShootingGuard
-let ballHandlerDescription = ballHandler.rawValue
